c32c9eaa7ed1409c4843b60e542e13d3e329f7a2
root <slashmod6@gmail.com>
2022-03-01 21:55:26 +0000

    final

--- apps/info/config.js
+++ apps/info/config.js
@@ -7,7 +7,6 @@ window.apps["info"]["tile"] = `<div class="box_widget">
 		<ul class="list">
 			<li><a href="apps/info/settings.html"l>Change Settings</a></li>
 			<li><a href="apps/info/info.html">Details on Bunker</a></li>
-			<li><a href="apps/info/guiapp.html">Install an app</a></li>
 			<li><a href="apps/info/about.html">Technical bunker://about</a></li>
 			<li class="italic">(Includes information on system and apps)</li>
 		</ul>
