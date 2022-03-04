a9d0b44c093d326d80f1ca2166afd2634a1b9176
Logan L <69491394+Cattn@users.noreply.github.com>
2022-03-04 07:34:51 -0500

    Delete apps directory

--- apps/prxyz/tile.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("prxyz_select").addEventListener("change", function(){
-	if(localStorage.prxyzPreflight==1){
-		goInternal("/apps/prxyz/get.html?id="+document.getElementById("prxyz_select").selectedIndex);
-	} else {
-		localStorage.prxyzPreflight=1;
-		goInternal("/apps/prxyz/preflight.html?id="+document.getElementById("prxyz_select").selectedIndex);
-	}
-});
